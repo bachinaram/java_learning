518b2f3875daa903e277c7799e0b44bad4d7c368
ram pratap bachina <ram.pratap@students.iiit.ac.in>
2023-07-27 21:24:54 +0530

    operators first program

---
+++ corejava/Java_operators_arithmetic.java
@@ -0,0 +1,11 @@
+
+class Java_operators_arithmetic{
+    public static void main(String[] args) {
+        int a=40;
+        int b=21;
+        System.out.println("Addition of variable a and b is:" + (a+b));    
+        System.out.println("Subtraction of variable a and b is:"+(b-a));
+        System.out.println("Division of variable a by b is:"+(a/b));
+        System.out.println("Remainder of variables a and b is:"+(a%b));
+    }
+}
