c580479190bbab27166aa6f452fa8bf43977433f
Ram <55042314+bachinaram@users.noreply.github.com>
2023-11-04 05:36:18 +0000

    search insert position leet ode easy array concept

---
+++ corejava/leetcode/arrays/search_insert_position.java
@@ -0,0 +1,47 @@
+package leetcode.arrays;
+
+public class search_insert_position {
+    public int searchInsert(int[] nums, int target) {
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                result = i;
+                return result;
+            }
+            else if(nums[i]>target){
+                result = i;
+                return result;
+            }
+            else if((nums[i]<target) && (i==nums.length-1)){
+                result = i+1;
+                return result;
+            }
+        }
+        return result;
+    }
+}
+
+
+/*
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+ */
