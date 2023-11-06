36f86d05727f482b9dec8ebfb5735f6bee3d9e5d
Ram <55042314+bachinaram@users.noreply.github.com>
2023-11-06 04:05:35 +0000

    strings practice valid paranthesis using stacks

---
+++ corejava/leetcode/strings/ValidParentheses.java
@@ -0,0 +1,59 @@
+package corejava.leetcode.strings;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false; // Unmatched closing bracket or extra opening bracket
+            }
+        }
+
+        return stack.isEmpty(); // The stack should be empty if all brackets are matched.
+    }
+
+    public static void main(String[] args) {
+        String input = "{[()]}";
+        boolean isValid = isValid(input);
+        System.out.println("Is the input string valid? " + isValid);
+    }
+}
+
+/*
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+ */
