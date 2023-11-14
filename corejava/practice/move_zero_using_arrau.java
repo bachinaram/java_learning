6b420da41ecc31b136b0332c1926c3d2bddf7ab0
Ram <55042314+bachinaram@users.noreply.github.com>
2023-11-14 05:09:51 +0000

    adding comments to the answers

--- corejava/practice/move_zero_using_arrau.java
+++ corejava/practice/move_zero_using_arrau.java
@@ -28,3 +28,19 @@ public class move_zero_using_arrau {
         System.out.println(output);
     }
 }
+/*
+ * 
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+
+
+Note that you must do this in-place without making a copy of the array.
+
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+
+ */
