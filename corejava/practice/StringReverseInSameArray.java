5a4a2d50a62573eebe743f5bf734f8b0e2274187
Ram <55042314+bachinaram@users.noreply.github.com>
2023-11-23 04:41:31 +0000

    String, array based questions

---
+++ corejava/practice/StringReverseInSameArray.java
@@ -0,0 +1,40 @@
+package practice;
+
+public class StringReverseInSameArray {
+    public static void main(String[] args){
+        char[] s = {'h','e','l','l','o'};
+        System.out.println(reverseString(s));
+    }
+    public static char[] reverseString(char[] s){
+        int start = 0;
+        int end = s.length-1;
+        char temp;
+        while(start<end){
+            temp = s[end];
+            s[end]=s[start];
+            s[start]=temp;
+            start++;
+            end--;
+        }
+        return s; 
+    }
+}
+
+/*
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+ */
